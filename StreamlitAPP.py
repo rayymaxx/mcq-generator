0c010c9e60ff87b4d3934e9fab7ffc2496dd2f2b
rayymaxx <rayymondodhiambo@gmail.com>
2025-07-03 16:36:21 +0300

    Made changes to the files

---
+++ StreamlitAPP.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("src")
+import os
+import json
+import traceback
+import pandas as pd
+from dotenv import load_dotenv
+from src.mcqgenerator.utils import read_file, get_table_data
+import streamlit as st
+from langchain_community.callbacks.manager import get_openai_callback
+from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
+from src.mcqgenerator.logger import logging
+
+
--- src/mcqgenerator/logger.py
+++ src/mcqgenerator/logger.py
@@ -17,3 +17,4 @@ logging.basicConfig(level=logging.INFO,
         format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
 )
 
+
